ed9515363c468c1879c72959685da896b926130a
Emmanuel Sobrevega <58387480+esobrevega@users.noreply.github.com>
2025-06-18 16:13:24 +0800

    Add .next/static/chunks/src_app_tickets_[id]_page_tsx_a993527f._.js 1750234402408

---
+++ .next/static/chunks/src_app_tickets_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_877524aa._.js",
+  "static/chunks/src_075756bb._.js"
+],
+    source: "dynamic"
+});
